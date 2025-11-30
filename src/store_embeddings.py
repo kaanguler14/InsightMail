ff83389c7bb87998b703044a854c0f133b7d75f4
Kaan Güler <glerkaan@yahoo.com>
2025-11-30 22:30:05 +0300

    Add store vector database

---
+++ src/store_embeddings.py
@@ -0,0 +1,35 @@
+from vector_database import QdrantStorage
+from src.Email_Parser import EmailParser
+from src.Email_Chunker import EmailChunker
+from src.Email_Embedding import Email_Embedding
+
+qdrant = QdrantStorage(
+    url="http://localhost:6333",
+    collection="emails",
+    dim=1024
+)
+parser = EmailParser("kaangulergs@gmail.com","ioue gqpu aekc zcbj")
+chunker = EmailChunker(parser)
+embedder = Email_Embedding(chunker,'Qwen/Qwen3-Embedding-0.6B')
+
+
+ids =[]
+vector=[]
+payloads=[]
+
+counter=0
+
+for item in embedder.embedding(batch_size=1024):
+
+    ids.append(counter)
+    vector.append(item["embedding"])
+    payloads.append({"text":item["text"],
+                     "source":item["source"]})
+    counter+=1
+
+    if len(ids)>=100:
+        qdrant.upsert(ids,vector,payloads)
+        ids,vectors,payloads=[],[],[]
+
+if ids:
+    qdrant.upsert(ids,vector,payloads)
